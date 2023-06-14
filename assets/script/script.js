f87b9904cbf60f320f00555e71f1ecaf2aa1142c
giovannipro <g.profet@gmail.com>
2023-06-14 16:59:37 +0200

    test the url parameter

--- assets/script/script.js
+++ assets/script/script.js
@@ -15,12 +15,36 @@ function manage_url(){
 
         urlParams.set('luogo', id);
         window.location.search = urlParams;
+
+        set_background(id)
+        console.log(id)
     }
+
 
 }
 
+function set_background(){
+    let color;
+
+    let search = new URLSearchParams(window.location.search);
+    let id = search.get('luogo');
+
+    if (id == 1) color = "red";
+    else if (id == 2) color = "lightblue";
+    else if (id == 3) color = "blue";
+    else if (id == 4) color = "purple";
+    else if (id == 5) color = "gray";
+    else color = "yellow";
+
+    const elem = document.getElementById("test");
+    elem.style.backgroundColor = color;
+    console.log(id,color)
+
+
+}
 
 window.addEventListener('load', function () {
-    // console.log('ready')
     manage_url()
+    set_background();
+
 })
