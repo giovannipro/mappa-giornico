f41f8154a746631447df9b40e010258e709ba146
giovannipro <g.profet@gmail.com>
2023-06-20 21:40:15 +0200

    load data from osm

--- assets/script/map.js
+++ assets/script/map.js
@@ -42,12 +42,32 @@ let activeIcon = L.icon({
     className: ""
 });
 
+let amenityIcon = L.icon({
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41],
+    className: ""
+});
 
 let currentId = 0;
 
 const images = [
     'assets/imgs/museo-leventina_1.jpg', 
 ]
+
+const osm_api = 'https://api.openstreetmap.org/api/0.6/map?bbox=-0.111110,51.111110,0.111111,51.111111'
+
+const query_a = '[out:json];area[name="Giornico"]->.searchArea;(way["amenity"~"parking|toilets|public_transport"](area.searchArea););out center;>;out skel qt;'
+const query_b = '[out:json];(way(around:1000,46.4019, 8.8752););out center;' // [amenity]
+const overpass_api = 'https://overpass-api.de/api/interpreter?data=' + query_b
+
+
+// relation["amenity"="parking"](area.searchArea)
+// node["amenity"="parking"](area.searchArea);  
+// console.log(overpass_api)
 
 // sidebar
 const sidebarA_container = document.getElementById("sidebarA_content");
@@ -95,6 +115,46 @@ function make_map(data){
         maxZoom: max_zoom,
         tileSize: 256
     }).addTo(map);
+
+    fetch(overpass_api)
+        .then(response => response.json())
+        .then(data => {
+            elements = data.elements
+            // console.log(elements)
+            
+            elements.forEach(item => {
+
+                // console.log(item)
+                lat = item.center.lat
+                lon = item.center.lon
+                // name = item.tags.amenity
+
+                if (item.tags){
+                    names = item.tags //  + ' ' + item.tags[1]
+
+                    name = ''
+                    Object.keys(names).forEach(function(key) {
+                        let value = names[key];
+                        name += key + ': ' + value + '<br/>'
+                    });
+                    name += '<br/>' + calculateDistance(lat, lon, 46.4019, 8.8752)
+
+                    // if (name !== 'parkinga'){
+                        const marker = L.circle([lat, lon], 2, {
+                            icon: amenityIcon
+                        })
+                        .bindPopup(name)
+                        .on('mouseover', function () {
+                            this.openPopup()
+                        })
+                        .on('mouseout', function () {
+                            this.closePopup()
+                        })
+                        .addTo(map)
+                    // }
+                }
+            })
+    });
 
     // fix data
     data.forEach(item => {
@@ -319,4 +379,33 @@ function set_url_param(id,name) {
 
     // replace the current URL without refreshing the page
     window.history.replaceState({}, '', newURL);
+}
+
+function calculateDistance(lat1, lon1, lat2, lon2) {
+  const earthRadius = 6371; // Radius of the Earth in kilometers
+
+  // Convert latitude and longitude to radians
+  const latRad1 = degToRad(lat1);
+  const lonRad1 = degToRad(lon1);
+  const latRad2 = degToRad(lat2);
+  const lonRad2 = degToRad(lon2);
+
+  // Calculate the differences between the coordinates
+  const latDiff = latRad2 - latRad1;
+  const lonDiff = lonRad2 - lonRad1;
+
+  // Apply the Haversine formula
+  const a =
+    Math.sin(latDiff / 2) ** 2 +
+    Math.cos(latRad1) * Math.cos(latRad2) * Math.sin(lonDiff / 2) ** 2;
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  const distanceKm = earthRadius * c;
+
+  const distanceM = distanceKm * 1000
+
+  return distanceM;
+}
+
+function degToRad(degrees) {
+  return degrees * (Math.PI / 180);
 }
