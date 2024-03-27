cbe0ce23c5f2f1ee740cec7cca45b02183b68bf1
giovannipro <g.profet@gmail.com>
2024-03-27 18:50:35 +0100

    prevent to show empty fields

--- assets/script/map.js
+++ assets/script/map.js
@@ -338,14 +338,20 @@ function update_sidebarB(id){
     // content
     content += '<div id="content">'
 
-    content += '<div class="info"><p class="parameter">Descrizione</p>'
-    content += '<p class="value">' + description + '</p></div>'
+    if (description != ""){
+        content += '<div class="info"><p class="parameter">Descrizione</p>'
+        content += '<p class="value">' + description + '</p></div>'
+    }
 
-    content += '<div class="info"><p class="parameter">Curiosità</p>'
-    content += '<p class="value">' + curiosity + '</p></div>'
+    if (curiosity != ""){
+        content += '<div class="info"><p class="parameter">Curiosità</p>'
+        content += '<p class="value">' + curiosity + '</p></div>'
+    }
 
-    content += '<div class="info"><p class="parameter">Esperienza sensoriale</p>'
-    content += '<p class="value">' + senses + '</p></div>'
+    if (senses != ""){
+        content += '<div class="info"><p class="parameter">Esperienza sensoriale</p>'
+        content += '<p class="value">' + senses + '</p></div>'
+    }
 
     content += '<div>'
 
@@ -430,33 +436,4 @@ function set_url_param(id,name) {
 
     // replace the current URL without refreshing the page
     window.history.replaceState({}, '', newURL);
-}
-
-function calculateDistance(lat1, lon1, lat2, lon2) {
-  const earthRadius = 6371; // Radius of the Earth in kilometers
-
-  // Convert latitude and longitude to radians
-  const latRad1 = degToRad(lat1);
-  const lonRad1 = degToRad(lon1);
-  const latRad2 = degToRad(lat2);
-  const lonRad2 = degToRad(lon2);
-
-  // Calculate the differences between the coordinates
-  const latDiff = latRad2 - latRad1;
-  const lonDiff = lonRad2 - lonRad1;
-
-  // Apply the Haversine formula
-  const a =
-    Math.sin(latDiff / 2) ** 2 +
-    Math.cos(latRad1) * Math.cos(latRad2) * Math.sin(lonDiff / 2) ** 2;
-  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-  const distanceKm = earthRadius * c;
-
-  const distanceM = distanceKm * 1000
-
-  return distanceM;
-}
-
-function degToRad(degrees) {
-  return degrees * (Math.PI / 180);
 }
--- assets/script/script.js
+++ assets/script/script.js
@@ -1,3 +1,32 @@
+function calculateDistance(lat1, lon1, lat2, lon2) {
+    const earthRadius = 6371; // Radius of the Earth in kilometers
+
+    // Convert latitude and longitude to radians
+    const latRad1 = degToRad(lat1);
+    const lonRad1 = degToRad(lon1);
+    const latRad2 = degToRad(lat2);
+    const lonRad2 = degToRad(lon2);
+
+    // Calculate the differences between the coordinates
+    const latDiff = latRad2 - latRad1;
+    const lonDiff = lonRad2 - lonRad1;
+
+    // Apply the Haversine formula
+    const a =
+    Math.sin(latDiff / 2) ** 2 +
+    Math.cos(latRad1) * Math.cos(latRad2) * Math.sin(lonDiff / 2) ** 2;
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const distanceKm = earthRadius * c;
+
+    const distanceM = distanceKm * 1000
+
+    return distanceM;
+}
+
+function degToRad(degrees) {
+    return degrees * (Math.PI / 180);
+}
+
 window.addEventListener('load', function () {
     load_data();
 })
